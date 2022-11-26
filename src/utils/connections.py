fa57c01f56ca8b3cb9d9bc0738d4d9c3ea2855b5
Nivaldo Carvalho <nivaldo_j@outlook.com>
2022-11-26 16:07:06 -0300

    Fixes to connection methods

--- src/utils/connections.py
+++ src/utils/connections.py
@@ -1,12 +1,13 @@
 from pyodbc import Connection, connect
 
-DRIVER_NAME: str = r'{ODBC Driver 17 for SQL Server}'
+DRIVER_NAME: str = 'ODBC Driver 17 for SQL Server'
 
 
 def connect_winauth(server_name: str,
-                    database_name: str) -> Connection:
+                    database_name: str,
+                    driver_name: str = DRIVER_NAME) -> Connection:
     connstring: str = (
-        f'Driver={DRIVER_NAME};'
+        f'Driver={driver_name};'
         f'Server={server_name};'
         f'Database={database_name};'
         f'Trusted_Connection=yes;'
@@ -18,14 +19,16 @@ def connect_sqlauth(server_ip: str,
                     server_port: str,
                     username: str,
                     password: str,
-                    database_name: str) -> Connection:
+                    database_name: str,
+                    driver_name: str = DRIVER_NAME) -> Connection:
     connstring: str = (
-        f'Driver={DRIVER_NAME};'
+        f'Driver={driver_name};'
         f'Server={server_ip};'
         f'port={server_port};'
         f'Network Library=DBMSSOCN;'
         f'Database={database_name};'
         f'uid={username};'
         f'pwd={password};'
+        f'TrustServerCertificate=yes;'
     )
     return connect(connstring)
